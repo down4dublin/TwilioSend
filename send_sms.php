407eeee01dbf2632edaa5e9c190b6bc8888d7989
Damina Townes <daminatownes@gmail.com>
2018-10-06 14:16:04 -0400

    Twilio
    add update

--- send_sms.php
+++ send_sms.php
@@ -1,12 +1,11 @@
 <?php
-
+echo ("getsms page ");
 // Update the path below to your autoload.php,
 // see https://getcomposer.org/doc/01-basic-usage.md
 
 require_once 'vendor/autoload.php';
 use Twilio\Rest\Client;
 
-// Find your Account Sid and Auth Token at twilio.com/console
 $sid    = "AC1ba67ccf30ea2fe309901290ea6937e3";
 $token  = "e1d0f772142d3e7acd08fd2ead3af7f7";
 $twilio = new Client($sid, $token);
